3f50eda771db201195b781f86ac7a0f9afad4ca6
marcotristani <marco.tristani@hotmail.it>
2026-01-22 14:38:22 +0100

    flat: add submit per aggiungere elemento alla lista

--- src/components/ListFilms.jsx
+++ src/components/ListFilms.jsx
@@ -7,21 +7,33 @@ const ListFilms = () => {
   //Variabili stringa film e variabile dove salvare nuovo valore del film
   const [titleFilms, setTitleFilms] = useState(filmsCheccoZalone);
   const [newTitleFilm, setNewTitleFilm] = useState("");
+
+  //Funzione di azione al submit per aggiungere nuovo film alla lista
+  function addFilm(event) {
+    event.preventDefault(); //blocco evento naturale pagina ossia il ricaricamento
+    if (newTitleFilm !== "") {
+      setTitleFilms([...titleFilms, newTitleFilm]); //vado ad aggiungere un nuovo film alla variabile di stato title film
+      setNewTitleFilm(""); //pulisco il testo di input
+    } else {
+      alert("Inserisci un titolo");
+    }
+  }
   return (
     <>
       <h2>Film</h2>
       <ul>
-        {filmsCheccoZalone.map((film) => (
+        {titleFilms.map((film) => (
           <li>{film}</li>
         ))}
       </ul>
-      <form>
+      <form onSubmit={addFilm}>
         <input
           type="text"
           placeholder="Inserisci un nuovo titolo"
           value={newTitleFilm}
           onChange={(e) => setNewTitleFilm(e.target.value)}
         ></input>
+        <button>Add</button>
       </form>
       {/* prova */}
       <p>{titleFilms}</p>
