e73aa55f29b4a5b3d09258892143444324c9f9d3
marcotristani <marco.tristani@hotmail.it>
2026-01-22 15:32:18 +0100

    flat: intallato fontawesome e definito bottone per eliminare lista

--- src/components/ListFilms.jsx
+++ src/components/ListFilms.jsx
@@ -1,4 +1,13 @@
-import { use } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import all the icons in Free Solid, Free Regular, and Brands styles */
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+
+library.add(fas, far, fab);
+
 import filmsCheccoZalone from "../assets/data/FilmsCheccoZalone";
 
 import { useState } from "react";
@@ -18,12 +27,22 @@ const ListFilms = () => {
       alert("Inserisci un titolo");
     }
   }
+
+  function removeFilm(index) {
+    const filmRimanenti = titleFilms.filter((film, i) => i !== index);
+    setTitleFilms(filmRimanenti);
+  }
   return (
     <>
       <h2>Film</h2>
       <ul>
-        {titleFilms.map((film) => (
-          <li>{film}</li>
+        {titleFilms.map((film, index) => (
+          <li key={index}>
+            {film}
+            <button className="delete-btn" onClick={() => removeFilm(index)}>
+              <FontAwesomeIcon icon="fa-solid fa-poo" />
+            </button>
+          </li>
         ))}
       </ul>
       <form onSubmit={addFilm}>
