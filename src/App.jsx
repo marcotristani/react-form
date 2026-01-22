b2f1e6df402a69140fd0c3072640254f30caafc0
marcotristani <marco.tristani@hotmail.it>
2026-01-22 14:28:12 +0100

    flat: creata lista con form senza submit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import ListFilms from "./components/ListFilms";
 
 function App() {
-  return <ListFilms />;
+  return (
+    <>
+      <header>
+        <h1>Checco Zalone</h1>
+      </header>
+      <main>
+        <ListFilms />
+      </main>
+    </>
+  );
 }
 
 export default App;
--- src/components/ListFilms.jsx
+++ src/components/ListFilms.jsx
@@ -1,6 +1,32 @@
+import { use } from "react";
 import filmsCheccoZalone from "../assets/data/FilmsCheccoZalone";
 
+import { useState } from "react";
+
 const ListFilms = () => {
-  return <h1>START</h1>;
+  //Variabili stringa film e variabile dove salvare nuovo valore del film
+  const [titleFilms, setTitleFilms] = useState(filmsCheccoZalone);
+  const [newTitleFilm, setNewTitleFilm] = useState("");
+  return (
+    <>
+      <h2>Film</h2>
+      <ul>
+        {filmsCheccoZalone.map((film) => (
+          <li>{film}</li>
+        ))}
+      </ul>
+      <form>
+        <input
+          type="text"
+          placeholder="Inserisci un nuovo titolo"
+          value={newTitleFilm}
+          onChange={(e) => setNewTitleFilm(e.target.value)}
+        ></input>
+      </form>
+      {/* prova */}
+      <p>{titleFilms}</p>
+      <p>{newTitleFilm}</p>
+    </>
+  );
 };
 export default ListFilms;
